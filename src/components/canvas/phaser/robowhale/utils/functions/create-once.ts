10f38183864c607fd6599eb8bceb9272c790f42f
Vladislav Forsh <vlad@robowhale.com>
2025-01-02 19:42:00 +0300

    add helper function - createOnce

---
+++ src/components/canvas/phaser/robowhale/utils/functions/create-once.ts
@@ -0,0 +1,31 @@
+type OnceCallback<T extends (...args: any[]) => void> = {
+	get wasCalled(): boolean
+	signal?: AbortSignal
+	call: T
+}
+
+export function createOnce<T extends (...args: any[]) => void>(fn: T, signal?: AbortSignal): OnceCallback<T> {
+	let result: ReturnType<T> | undefined
+    let wasCalled = false
+	const callback: OnceCallback<T> = {
+		get wasCalled(): boolean {
+			return wasCalled
+		},
+		signal,
+		call: ((...args: Parameters<T>) => {
+			if (signal?.aborted) {
+				return result
+			}
+			
+			if (wasCalled) {
+				return result
+			}
+			
+			wasCalled = true
+			result = fn(...args) as ReturnType<T>
+			return result
+		}) as unknown as T,
+	}
+
+	return callback
+}
