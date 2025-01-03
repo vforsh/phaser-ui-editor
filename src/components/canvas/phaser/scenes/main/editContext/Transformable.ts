77364dab20ca028d6a33887343cb5edbe5545c64
Vladislav Forsh <vlad@robowhale.com>
2025-01-03 19:52:43 +0300

    update selection bounds calculation

--- src/components/canvas/phaser/scenes/main/editContext/Transformable.ts
+++ src/components/canvas/phaser/scenes/main/editContext/Transformable.ts
@@ -9,25 +9,72 @@ export function isTransformable(go: Phaser.GameObjects.GameObject): go is Transf
 }
 
 export function calculateBounds(objects: Transformable[], rect?: Phaser.Geom.Rectangle): Phaser.Geom.Rectangle {
-	const left = objects.reduce((min, obj) => {
-		const left = obj.x - (obj.getData('originX') ?? obj.originX) * obj.displayWidth
-		return Math.min(min, left)
-	}, Infinity)
+	if (objects.length === 1) {
+		return calculateBoundsSingle(objects[0], rect)
+	}
 
-	const right = objects.reduce((max, obj) => {
-		const right = obj.x + (1 - (obj.getData('originX') ?? obj.originX)) * obj.displayWidth
-		return Math.max(max, right)
-	}, -Infinity)
+	const point = new Phaser.Math.Vector2()
+	const offset = new Phaser.Math.Vector2()
+	let left = Infinity,
+		right = -Infinity,
+		top = Infinity,
+		bottom = -Infinity
 
-	const top = objects.reduce((min, obj) => {
-		const top = obj.y - (obj.getData('originY') ?? obj.originY) * obj.displayHeight
-		return Math.min(min, top)
-	}, Infinity)
+	objects.forEach((obj) => {
+		const sin = Math.sin(obj.rotation)
+		const cos = Math.cos(obj.rotation)
+		const originX = obj.getData('originX') ?? obj.originX
+		const originY = obj.getData('originY') ?? obj.originY
 
-	const bottom = objects.reduce((max, obj) => {
-		const bottom = obj.y + (1 - (obj.getData('originY') ?? obj.originY)) * obj.displayHeight
-		return Math.max(max, bottom)
-	}, -Infinity)
+		// Top-left
+		point.set(obj.x - obj.displayWidth * originX, obj.y - obj.displayHeight * originY)
+		offset.set(point.x - obj.x, point.y - obj.y)
+		point.set(obj.x + (offset.x * cos - offset.y * sin), obj.y + (offset.x * sin + offset.y * cos))
+		left = Math.min(left, point.x)
+		right = Math.max(right, point.x)
+		top = Math.min(top, point.y)
+		bottom = Math.max(bottom, point.y)
+
+		// Top-right
+		point.set(obj.x + obj.displayWidth * (1 - originX), obj.y - obj.displayHeight * originY)
+		offset.set(point.x - obj.x, point.y - obj.y)
+		point.set(obj.x + (offset.x * cos - offset.y * sin), obj.y + (offset.x * sin + offset.y * cos))
+		left = Math.min(left, point.x)
+		right = Math.max(right, point.x)
+		top = Math.min(top, point.y)
+		bottom = Math.max(bottom, point.y)
+
+		// Bottom-left
+		point.set(obj.x - obj.displayWidth * originX, obj.y + obj.displayHeight * (1 - originY))
+		offset.set(point.x - obj.x, point.y - obj.y)
+		point.set(obj.x + (offset.x * cos - offset.y * sin), obj.y + (offset.x * sin + offset.y * cos))
+		left = Math.min(left, point.x)
+		right = Math.max(right, point.x)
+		top = Math.min(top, point.y)
+		bottom = Math.max(bottom, point.y)
+
+		// Bottom-right
+		point.set(obj.x + obj.displayWidth * (1 - originX), obj.y + obj.displayHeight * (1 - originY))
+		offset.set(point.x - obj.x, point.y - obj.y)
+		point.set(obj.x + (offset.x * cos - offset.y * sin), obj.y + (offset.x * sin + offset.y * cos))
+		left = Math.min(left, point.x)
+		right = Math.max(right, point.x)
+		top = Math.min(top, point.y)
+		bottom = Math.max(bottom, point.y)
+	})
+
+	if (rect) {
+		return rect.setTo(left, top, right - left, bottom - top)
+	}
+
+	return new Phaser.Geom.Rectangle(left, top, right - left, bottom - top)
+}
+
+function calculateBoundsSingle(obj: Transformable, rect?: Phaser.Geom.Rectangle): Phaser.Geom.Rectangle {
+	const left = obj.x - (obj.getData('originX') ?? obj.originX) * obj.displayWidth
+	const right = obj.x + (1 - (obj.getData('originX') ?? obj.originX)) * obj.displayWidth
+	const top = obj.y - (obj.getData('originY') ?? obj.originY) * obj.displayHeight
+	const bottom = obj.y + (1 - (obj.getData('originY') ?? obj.originY)) * obj.displayHeight
 
 	if (rect) {
 		return rect.setTo(left, top, right - left, bottom - top)
