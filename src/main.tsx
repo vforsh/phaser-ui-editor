8be2ecc7b442b964ab4b8fb656a1877bbdcb6eb8
Vladislav Forsh <vlad@robowhale.com>
2025-01-21 21:00:33 +0300

    disable react scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
 // should be imported BEFORE react-dom/client
-import { scan } from 'react-scan'
+// import { scan } from 'react-scan'
 
 // should be imported AFTER react-scan
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './polyfills'
-import { urlParams } from '@url-params'
 
-if (urlParams.getBool('scan')) {
-	scan({
-		enabled: true,
-		log: false, // logs render info to console (default: false)
-	})
-}
+// scan({
+// 	enabled: true,
+// 	log: false, // logs render info to console (default: false)
+// })
 
 createRoot(document.getElementById('root')!).render(
 	// <StrictMode>
