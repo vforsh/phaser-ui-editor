05223b915d334fd6c936508e2d1e1962686f4da9
Vladislav Forsh <vlad@robowhale.com>
2025-02-14 14:01:35 +0300

    add script to generate d.ts files for game

---
+++ scripts/build-types.ts
@@ -0,0 +1,52 @@
+/**
+ * This script generates TypeScript type declarations (.d.ts files) for the project.
+ *
+ * It performs the following steps:
+ *
+ * 1. Reads the tsconfig.dts.json configuration file
+ * 2. Runs the TypeScript compiler (tsc) to generate initial .d.ts files
+ * 3. Uses dtsroll to bundle all the declarations into a single file
+ * 4. Copies the bundled declarations to the output directory as index.d.ts
+ *
+ * The generated type declarations allow the project to be used as a typed dependency in other TypeScript projects.
+ */
+
+import { execSync } from 'child_process'
+import fse from 'fs-extra'
+import { join } from 'path'
+import type { CompilerOptions } from 'typescript'
+
+interface TsConfig {
+	compilerOptions?: CompilerOptions
+}
+
+async function main() {
+	try {
+		const tsconfigPath = join(process.cwd(), 'tsconfig.dts.json')
+		const tscConfig = fse.readJsonSync(tsconfigPath, 'utf8') as TsConfig
+		const outDir = tscConfig.compilerOptions!.declarationDir!
+
+		const exportsPath = join('src', 'types', 'exports', 'exports.d.ts')
+		const exportsDtsPath = join(outDir, exportsPath)
+
+		// Run tsc to generate initial d.ts files
+		console.log('Generating type declarations...')
+		execSync('npx tsc -p tsconfig.dts.json', { stdio: 'inherit' })
+
+		// Run dtsroll to bundle the declarations
+		console.log('Bundling type declarations...')
+		execSync(`npx dtsroll ${exportsDtsPath}`, { stdio: 'inherit' })
+
+		const exportsContent = await fse.readFile(exportsDtsPath, 'utf8')
+		const indexPath = join(outDir, 'index.d.ts')
+		await fse.outputFile(indexPath, exportsContent)
+
+		console.log(``)
+		console.log(`Type declarations generated successfully at ${indexPath}!`)
+	} catch (error) {
+		console.error('Error generating type declarations:', error)
+		process.exit(1)
+	}
+}
+
+main()
---
+++ src/types/exports/exports.ts
@@ -0,0 +1,30 @@
+// it is important to use the relative paths in imports, not path aliases like '@components/*'
+// because the types are exported as a .d.ts file and need to be available for the consumers of the library
+
+import { EditableComponentJson } from '../../components/canvas/phaser/scenes/main/objects/components/base/EditableComponent'
+import { GridLayoutComponentJson } from '../../components/canvas/phaser/scenes/main/objects/components/GridLayoutComponent'
+import { HorizontalLayoutComponentJson } from '../../components/canvas/phaser/scenes/main/objects/components/HorizontalLayoutComponent'
+import { PinnerComponentJson } from '../../components/canvas/phaser/scenes/main/objects/components/PinnerComponent'
+import { VerticalLayoutComponentJson } from '../../components/canvas/phaser/scenes/main/objects/components/VerticalLayoutComponent'
+import { EditableBitmapTextJson } from '../../components/canvas/phaser/scenes/main/objects/EditableBitmapText'
+import { EditableContainerJson } from '../../components/canvas/phaser/scenes/main/objects/EditableContainer'
+import { EditableImageJson } from '../../components/canvas/phaser/scenes/main/objects/EditableImage'
+import { EditableNineSliceJson } from '../../components/canvas/phaser/scenes/main/objects/EditableNineSlice'
+import { EditableObjectJson } from '../../components/canvas/phaser/scenes/main/objects/EditableObject'
+import { EditableTextJson } from '../../components/canvas/phaser/scenes/main/objects/EditableText'
+import { PrefabFile } from '../prefabs/PrefabFile'
+
+export type {
+	EditableBitmapTextJson,
+	EditableComponentJson,
+	EditableContainerJson,
+	EditableImageJson,
+	EditableNineSliceJson,
+	EditableObjectJson,
+	EditableTextJson,
+	GridLayoutComponentJson,
+	HorizontalLayoutComponentJson,
+	PinnerComponentJson,
+	PrefabFile,
+	VerticalLayoutComponentJson,
+}
