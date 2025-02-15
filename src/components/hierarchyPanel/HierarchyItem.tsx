6e5f2acc84d0e5fbf4adf1bc3a9c408ca6a7ce69
Vladislav Forsh <vlad@robowhale.com>
2025-02-15 20:30:08 +0300

    refactor

--- src/components/hierarchyPanel/HierarchyItem.tsx
+++ src/components/hierarchyPanel/HierarchyItem.tsx
@@ -1,18 +1,190 @@
 import { EditableObjectJson } from '@components/canvas/phaser/scenes/main/objects/EditableObject'
 import { ActionIcon, Group, Text, Tooltip, useMantineTheme } from '@mantine/core'
 import { useWindowEvent } from '@mantine/hooks'
-import { state } from '@state/State'
+import { notifications } from '@mantine/notifications'
+import { until } from '@open-draft/until'
+import { state, unproxy } from '@state/State'
+import { getErrorLog } from '@utils/error/utils'
 import clsx from 'clsx'
-import { ChevronDown, Eye, EyeOff, FolderSearch, Lock, Unlock } from 'lucide-react'
-import { ContextMenuOptions, useContextMenu } from 'mantine-contextmenu'
+import {
+	ChevronDown,
+	ChevronRight,
+	ClipboardCopy,
+	ClipboardPaste,
+	Copy,
+	Eye,
+	EyeOff,
+	FilePlus2,
+	FolderSearch,
+	Lock,
+	Scissors,
+	TextCursorInput,
+	Trash2,
+	Unlock,
+} from 'lucide-react'
+import { ContextMenuItemOptions, ContextMenuOptions, useContextMenu } from 'mantine-contextmenu'
 import { useMemo, useState } from 'react'
-import { useSnapshot } from 'valtio'
-import { createHierarchyItemContextMenuItems } from '../hierarchyPanel/HierarchyPanel'
+import { Snapshot, useSnapshot } from 'valtio'
 import styles from './HierarchyItem.module.css'
+import { HIERARCHY_ITEMS_CONTAINER_ID } from './HierarchyPanel'
 import { getHierarchyItemIcon, getLinkedAssetId } from './hierarchyUtils'
 
 const INDENT_SIZE = 26
 const ICON_MARGIN = 8
+
+function createContextMenuItems(obj: Snapshot<EditableObjectJson>, isRoot = false): ContextMenuItemOptions[] {
+	let dividers = 1
+	const divider = () => {
+		return { key: `divider-${dividers++}` }
+	}
+
+	const appCommands = state.app?.commands
+
+	const linkedAssetId = getLinkedAssetId(obj as EditableObjectJson, isRoot)
+
+	const items: ContextMenuItemOptions[] = [
+		{
+			key: 'create',
+			icon: <FilePlus2 size={16} />,
+			title: 'Create',
+			iconRight: <ChevronRight size={14} />,
+			items: [
+				{
+					key: 'create-container',
+					icon: getHierarchyItemIcon('Container'),
+					title: 'Container',
+					onClick: () => {
+						appCommands?.emit('create-object', { clickedObjId: obj.id, type: 'Container' })
+					},
+				},
+			],
+		},
+		divider(),
+		{
+			key: 'copy',
+			title: 'Copy',
+			icon: <ClipboardCopy size={16} />,
+			onClick: () => {
+				appCommands?.emit('copy-object', obj.id)
+			},
+		},
+		{
+			key: 'duplicate',
+			title: 'Duplicate',
+			icon: <Copy size={16} />,
+			disabled: isRoot,
+			onClick: () => {
+				appCommands?.emit('duplicate-object', obj.id)
+			},
+		},
+		{
+			key: 'cut',
+			title: 'Cut',
+			icon: <Scissors size={16} />,
+			disabled: isRoot,
+			onClick: () => {
+				appCommands?.emit('cut-object', obj.id)
+			},
+		},
+		{
+			key: 'paste',
+			title: 'Paste',
+			icon: <ClipboardPaste size={16} />,
+			onClick: () => {
+				appCommands?.emit('paste-object', obj.id)
+			},
+		},
+		divider(),
+		{
+			key: 'locate-in-assets',
+			title: 'Locate in Assets',
+			icon: <FolderSearch size={16} />,
+			disabled: !linkedAssetId,
+			onClick: () => {
+				state.assets.locateAsset?.(linkedAssetId!)
+			},
+		},
+		divider(),
+		{
+			key: 'rename',
+			title: 'Rename',
+			icon: <TextCursorInput size={16} />,
+			onClick: () => {
+				console.log(`rename`, unproxy(obj))
+			},
+		},
+		{
+			key: 'delete',
+			title: 'Delete',
+			icon: <Trash2 size={16} />,
+			color: 'red',
+			disabled: isRoot,
+			onClick: (event) => {
+				appCommands?.emit('delete-object', obj.id)
+			},
+		},
+		divider(),
+		{
+			key: 'copy-id',
+			title: 'Copy ID',
+			icon: <ClipboardCopy size={16} />,
+			onClick: async () => {
+				const { error } = await until(() => navigator.clipboard.writeText(obj.id))
+
+				if (error) {
+					notifications.show({
+						title: 'Failed to copy ID',
+						message: `${getErrorLog(error)}`,
+						color: 'red',
+						autoClose: 10_000,
+					})
+
+					return
+				}
+
+				notifications.show({
+					title: 'ID Copied',
+					message: `${obj.id} copied to clipboard`,
+					color: 'green',
+					autoClose: 10_000,
+				})
+			},
+		},
+		{
+			key: 'copy-path',
+			title: 'Copy Path',
+			icon: <ClipboardCopy size={16} />,
+			onClick: async () => {
+				const path = appCommands?.emit('get-object-path', obj.id)
+				if (!path) {
+					return
+				}
+
+				const { error } = await until(() => navigator.clipboard.writeText(path))
+
+				if (error) {
+					notifications.show({
+						title: 'Failed to copy path',
+						message: `${getErrorLog(error)}`,
+						color: 'red',
+						autoClose: 10_000,
+					})
+
+					return
+				}
+
+				notifications.show({
+					title: 'Path Copied',
+					message: `${path} copied to clipboard`,
+					color: 'green',
+					autoClose: 10_000,
+				})
+			},
+		},
+	]
+
+	return items
+}
 
 interface HierarchyItemProps {
 	objState: EditableObjectJson
@@ -58,7 +230,9 @@ export default function HierarchyItem({
 
 	const getVisibleItems = (): EditableObjectJson[] => {
 		// find all dom elements with id starting with 'hierarchy-item-'
-		const items = document.querySelectorAll('[id^="hierarchy-item-"]') as NodeListOf<HTMLDivElement>
+		const items = document.querySelectorAll(
+			`#${HIERARCHY_ITEMS_CONTAINER_ID} [id^="hierarchy-item-"]`
+		) as NodeListOf<HTMLDivElement>
 
 		const objs: EditableObjectJson[] = []
 
@@ -294,7 +468,7 @@ export default function HierarchyItem({
 				onMouseLeave={() => setIsHovered(false)}
 				onContextMenu={(e) => {
 					e.preventDefault()
-					const menuItems = createHierarchyItemContextMenuItems(objState, isRoot)
+					const menuItems = createContextMenuItems(objState, isRoot)
 					const menuOptions: ContextMenuOptions = { style: { width: 200 } }
 					showContextMenu(menuItems, menuOptions)(e)
 					state.app?.commands.emit('select-object', objId)
--- src/components/hierarchyPanel/HierarchyPanel.tsx
+++ src/components/hierarchyPanel/HierarchyPanel.tsx
@@ -1,202 +1,13 @@
 import { Divider, Paper, ScrollArea, Stack } from '@mantine/core'
 import { useWindowEvent } from '@mantine/hooks'
-import { notifications } from '@mantine/notifications'
-import { until } from '@open-draft/until'
-import { state, unproxy, useSnapshot } from '@state/State'
-import { getErrorLog } from '@utils/error/utils'
-import {
-	ChevronRight,
-	ClipboardCopy,
-	ClipboardPaste,
-	Copy,
-	FilePlus2,
-	FolderSearch,
-	Scissors,
-	TextCursorInput,
-	Trash2,
-} from 'lucide-react'
-import { ContextMenuItemOptions } from 'mantine-contextmenu'
+import { state, useSnapshot } from '@state/State'
 import { useRef, useState } from 'react'
 import { Logger } from 'tslog'
-import { Snapshot } from 'valtio'
-import { EditableContainerJson, EditableObjectJson } from '../../types/exports/exports'
 import HierarchyItem from './HierarchyItem'
 import styles from './HierarchyPanel.module.css'
 import { HierarchyPanelTitle } from './HierarchyPanelTitle'
-import { getHierarchyItemIcon, getLinkedAssetId } from './hierarchyUtils'
 
-export function createHierarchyItemContextMenuItems(
-	obj: Snapshot<EditableObjectJson>,
-	isRoot = false
-): ContextMenuItemOptions[] {
-	let dividers = 1
-	const divider = () => {
-		return { key: `divider-${dividers++}` }
-	}
-
-	const appCommands = state.app?.commands
-
-	const linkedAssetId = getLinkedAssetId(obj as EditableObjectJson, isRoot)
-
-	const items: ContextMenuItemOptions[] = [
-		{
-			key: 'create',
-			icon: <FilePlus2 size={16} />,
-			title: 'Create',
-			iconRight: <ChevronRight size={14} />,
-			items: [
-				{
-					key: 'create-container',
-					icon: getHierarchyItemIcon('Container'),
-					title: 'Container',
-					onClick: () => {
-						appCommands?.emit('create-object', { clickedObjId: obj.id, type: 'Container' })
-					},
-				},
-			],
-		},
-		divider(),
-		{
-			key: 'copy',
-			title: 'Copy',
-			icon: <ClipboardCopy size={16} />,
-			onClick: () => {
-				appCommands?.emit('copy-object', obj.id)
-			},
-		},
-		{
-			key: 'duplicate',
-			title: 'Duplicate',
-			icon: <Copy size={16} />,
-			disabled: isRoot,
-			onClick: () => {
-				appCommands?.emit('duplicate-object', obj.id)
-			},
-		},
-		{
-			key: 'cut',
-			title: 'Cut',
-			icon: <Scissors size={16} />,
-			disabled: isRoot,
-			onClick: () => {
-				appCommands?.emit('cut-object', obj.id)
-			},
-		},
-		{
-			key: 'paste',
-			title: 'Paste',
-			icon: <ClipboardPaste size={16} />,
-			onClick: () => {
-				appCommands?.emit('paste-object', obj.id)
-			},
-		},
-		divider(),
-		{
-			key: 'locate-in-assets',
-			title: 'Locate in Assets',
-			icon: <FolderSearch size={16} />,
-			disabled: !linkedAssetId,
-			onClick: () => {
-				state.assets.locateAsset?.(linkedAssetId!)
-			},
-		},
-		divider(),
-		{
-			key: 'rename',
-			title: 'Rename',
-			icon: <TextCursorInput size={16} />,
-			onClick: () => {
-				console.log(`rename`, unproxy(obj))
-			},
-		},
-		{
-			key: 'delete',
-			title: 'Delete',
-			icon: <Trash2 size={16} />,
-			color: 'red',
-			disabled: isRoot,
-			onClick: (event) => {
-				appCommands?.emit('delete-object', obj.id)
-			},
-		},
-		divider(),
-		{
-			key: 'copy-id',
-			title: 'Copy ID',
-			icon: <ClipboardCopy size={16} />,
-			onClick: async () => {
-				const { error } = await until(() => navigator.clipboard.writeText(obj.id))
-
-				if (error) {
-					notifications.show({
-						title: 'Failed to copy ID',
-						message: `${getErrorLog(error)}`,
-						color: 'red',
-						autoClose: 10_000,
-					})
-
-					return
-				}
-
-				notifications.show({
-					title: 'ID Copied',
-					message: `${obj.id} copied to clipboard`,
-					color: 'green',
-					autoClose: 10_000,
-				})
-			},
-		},
-		{
-			key: 'copy-path',
-			title: 'Copy Path',
-			icon: <ClipboardCopy size={16} />,
-			onClick: async () => {
-				const path = appCommands?.emit('get-object-path', obj.id)
-				if (!path) {
-					return
-				}
-
-				const { error } = await until(() => navigator.clipboard.writeText(path))
-
-				if (error) {
-					notifications.show({
-						title: 'Failed to copy path',
-						message: `${getErrorLog(error)}`,
-						color: 'red',
-						autoClose: 10_000,
-					})
-
-					return
-				}
-
-				notifications.show({
-					title: 'Path Copied',
-					message: `${path} copied to clipboard`,
-					color: 'green',
-					autoClose: 10_000,
-				})
-			},
-		},
-	]
-
-	return items
-}
-
-export function flattenHierarchy(root: EditableContainerJson): EditableObjectJson[] {
-	const items: EditableObjectJson[] = []
-
-	items.push(root)
-
-	for (const child of root.children) {
-		if (child.type === 'Container') {
-			items.push(...flattenHierarchy(child))
-		} else {
-			items.push(child)
-		}
-	}
-
-	return items
-}
+export const HIERARCHY_ITEMS_CONTAINER_ID = 'hierarchy-items-container'
 
 export type HierarchyPanelProps = {
 	logger: Logger<{}>
@@ -269,7 +80,7 @@ export default function HierarchyPanel(props: HierarchyPanelProps) {
 						}
 					}}
 				>
-					<Stack gap={0}>
+					<Stack gap={0} id={HIERARCHY_ITEMS_CONTAINER_ID}>
 						{rootState && (
 							<HierarchyItem
 								key={rootState.id}
