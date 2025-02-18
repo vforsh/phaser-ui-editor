c360e3734d67f7a0e035983ee4637410ebcf1960
Vladislav Forsh <vlad@robowhale.com>
2025-02-18 21:10:48 +0300

    move react-scan to script tag

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,7 @@
-// should be imported BEFORE react-dom/client
-// import { scan } from 'react-scan'
-
-// should be imported AFTER react-scan
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './polyfills'
-
-// scan({
-// 	enabled: true,
-// 	log: false, // logs render info to console (default: false)
-// })
 
 createRoot(document.getElementById('root')!).render(
 	// <StrictMode>
